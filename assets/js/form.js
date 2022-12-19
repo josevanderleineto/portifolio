a02978edfa6929a7c68a684401490be58b7fa6c3
Vanderlei Neto <110213977+josevanderleineto@users.noreply.github.com>
2022-12-19 02:33:14 -0300

    Adicinado formulario

---
+++ assets/js/form.js
@@ -0,0 +1,79 @@
+class formSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute('action');
+        }
+
+        this.sendForm = this.sendForm.bind(this);
+    }
+
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    } 
+
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll('[name]');
+        fields.forEach((field) => {
+            formObject[field.getAttribute('name')] = field.value;
+        });
+
+        return formbject;
+    }
+
+    onSubmission(event) {
+        event.preventDefault();
+        event.target.disabled = true;
+        event.target.innerHTML = 'Enviand... '
+    }
+
+   async sendForm(event) {
+    try {
+
+        this.onSubmission(event);
+
+    await  
+    
+    fetch(this.url, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(this.formObject()),
+        });
+        this.displaySuccess();
+
+    } catch (error) {
+
+        this.displayError();
+
+        throw new Error(error);
+    }
+
+    }
+
+    init() {
+        if (this.form) 
+        this.formButton.addEventListener('click', this.sendForm);
+        return this;
+    }
+}
+
+const formSubmit = new FormSubmit({
+    form: '[data-form]',
+    button: '[data-button]',
+    success: '<h1 class="success">Mesagem enviada!</h1> ',
+    error: '<h1 class="error">Não foi possível enviar sua mensagem.</h1>',
+});
+
+formSubmit.init();
